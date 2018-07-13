d01c5f0635ed9830addd12175b792622c47ae93b
A Gautreaux <armando.gautreaux@icloud.com>
2018-07-13 14:33:53 -0500

    Added a new for loop to generate 10 images from the API url, added comments to html to better organize my work and precise some details in css

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,33 +6,30 @@ var giftsPlaceHolder = $('#gifts-placeholder');
 
 // 2.- Animals Array that we are going to use to autogenerate buttons
 var animals = [
-  'cats',
-  'mice',
-  'dogs',
-  'birds',
-  'cows',
+  'dog',
+  'mouse',
+  'cat',
+  'bird',
+  'cow',
   'bear',
-  'lions',
+  'lion',
   'sealion',
-  'zebra',
-  'startfish',
-  'turtle',
-  'ant'
+  'startfish'
 ];
 
-// 4. This function will generate each button of every element of Animals Array
+// 3. This function will generate each button of every element of Animal's Array
 function renderButtons() {
   btnsPlaceHolder.empty();
   for (var i = 0; i < animals.length; i++) {
     var button = $('<button>');
-    button.addClass('animal btn btn-info m-2');
+    button.addClass('animal btn btn-dark btn-lg text-white m-2');
     button.attr('data-name', animals[i]);
     button.text(animals[i]);
     btnsPlaceHolder.append(button);
   }
 }
 
-// 5.- This function (attached to an on.click event) will generate new buttons according to the user input
+// 4.- This function (attached to an on.click event) will generate new buttons according to the user input
 btnSubmit.on('click', function(event) {
   event.preventDefault();
   var addAnimal = inputAdd
@@ -46,8 +43,10 @@ btnSubmit.on('click', function(event) {
   }
 });
 
-// 5.- This function (attached to an on.click event) will perform an AJAX GET request to autogenerate images for every button that will click.
+// 5.- This function (attached to an on.click event) will perform an AJAX GET request to autogenerate images for every button that the user clicks.
 $(document.body).on('click', '.animal', function() {
+  giftsPlaceHolder.empty();
+
   // 5.1 We start by storing our Giphy API URL and our specific element to be call (in this case the name of the animals that we generate before)
   var animalName = $(this).attr('data-name');
   var queryURL =
@@ -60,21 +59,28 @@ $(document.body).on('click', '.animal', function() {
     url: queryURL,
     method: 'GET'
   }).then(function(response) {
-    var imgUrl = response.data[0].images.fixed_height_still.url;
-    var dataStill = response.data[0].images.fixed_height_still.url;
-    var dataAnimate = response.data[0].images.fixed_height.url;
-    var animalImg = $('<img/>');
-    animalImg.attr('src', imgUrl);
-    animalImg.attr('data-state', 'still');
-    animalImg.attr('data-still', dataStill);
-    animalImg.attr('data-animate', dataAnimate);
-    animalImg.attr('alt', 'animal image');
-    animalImg.addClass('w-25 m-2 gif');
-    giftsPlaceHolder.append(animalImg);
+    var results = response.data;
+    for (var i = 0; i < results.length; i++) {
+      var pg = $('<p>').text('Rating: ' + results[i].rating);
+      var imgUrl = results[i].images.fixed_height_still.url;
+      var dataStill = results[i].images.fixed_height_still.url;
+      var dataAnimate = results[i].images.fixed_height.url;
+      var animalImg = $('<img>');
+      animalImg.attr('src', imgUrl);
+      animalImg.attr('data-still', dataStill);
+      animalImg.attr('data-animate', dataAnimate);
+      animalImg.attr('data-state', 'still');
+      animalImg.attr('alt', animalName);
+      animalImg.addClass('gif img-fluid h-75 mt-2');
+      var animalDiv = $('<div>');
+      animalDiv.addClass('col-sm-3');
+      animalDiv.append(pg, animalImg);
+      giftsPlaceHolder.append(animalDiv);
+    }
   });
 });
 
-// 5.- This last function (attached to another on.click event) will allow us to change our image from still to animate everytime that is click it.
+// 6.- This last function (attached to another on.click event) will allow us to change our image from still to animate everytime that is click it.
 $(document.body).on('click', '.gif', function() {
   var state = $(this).attr('data-state');
   if (state === 'still') {
